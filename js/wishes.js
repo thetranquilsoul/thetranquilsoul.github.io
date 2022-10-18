90b67fb2754ef07ee42e34f7f7468b2ba134576b
vabbydante@gmail.com <vabbydante@gmail.com>
2022-10-18 20:21:17 +0530

    Initial Commit

---
+++ js/wishes.js
@@ -0,0 +1,14 @@
+//jshint esversion:6
+
+let x;
+
+$(".popup-btn").click(function (event) {
+    x = event.currentTarget.getAttribute("href");
+    $(x).addClass("zoomIn");
+    $(x).removeClass("zoomOut");
+});
+
+$(".close__box").click(function () {
+    $(x).removeClass("zoomIn");
+    $(x).addClass("zoomOut");
+});
